0007b2b76188aa4eb4e7e1b5fd9dee6d3210a8db
diegofmendes <diegoferreiramendes011@gmail.com>
2024-11-18 11:31:38 -0300

    Add files via upload
    Code design updates

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const timeEL = document.getElementById('timer');
+const marksList = document.getElementById('marks-list');
+let intervalld = 0;
+let timer = 0;
+let marks = [];
+
+
+
+    let hours = Math.floor 
+    let minutes = Math.floor
+    let seconds = Math.floor 
+    let miliseconds = Math.floor
